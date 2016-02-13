38a8b1deb51efe8f181379332fe02f090b050922
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-02-13 13:30:46 -0800

    Finish get and post routes

--- get-post.js
+++ get-post.js
@@ -7,11 +7,20 @@ reference, I did not cite every similarity because there
 are only so many ways to do these things :)
 *********************************************************/
 
+/*******************
+* Setup middleware *
+********************/
+
 // Import express
 var express = require('express');
 var app = express();
 
-// Set up handlebars
+// Import body-parser / setup (middleware for parsing POST content)
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// Import and set up handlebars
 var handlebars = require('express-handlebars').create({
     defaultLayout:'main',
     helpers: {
@@ -23,26 +32,50 @@ var handlebars = require('express-handlebars').create({
         }
     }
 });
-
+// Tell express our default rendering engine and extensions
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 // Set port and public folder for static content
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(__dirname + 'public'));
-// form processing:
-// app.use(require('body-parser')()); // makes req.body available
+app.set('port', process.env.PORT || 3000); // defaults to 3000 if env.port not set
+app.use(express.static(__dirname + '/public')); // tells express where to go for public static content like css, js, imgs, etc
 
 
 
-/******************
-* ROUTES:         *
-*******************/
 
-// Home page
+/*******************
+* ROUTES:          *
+********************/
+
+// If a GET request is received, grab the object and render the get.handlebars page
 app.get('/', function(req,res){
-    res.render('home');
+    var getRequest = [];
+    // Iterate over the req.query and push the key value pairs into our array
+    for (var key in req.query) {
+        getRequest.push( {'key': key, 'value': req.query[key] });
+    }
+    
+    // Make a 'content' object and pass it to our render page with a kvPairs[] property
+    var content = {};
+    content.kvPairs = getRequest;
+    res.render('get', content);
 });
+
+// If a POST request is received, parse the query or jsson and render the post.handlebars page
+app.post('/', function(req, res){
+    var postRequest = [];
+    // Iterate over the req.query and push the key value pairs into our array
+    for (var key in req.body) {
+        postRequest.push( {'key': key, 'value': req.body[key] });
+    }
+
+    // Make a 'content' object and pass it to our render page with a kvPairs[] property
+    var content = {};
+    content.kvPairs = postRequest;
+    res.render('post', content);
+});
+
+
 
 
 // Error Pages 404 (Not found) & 500 (Error)
