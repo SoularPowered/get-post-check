b43711b291d52735742ba6969b928bc2f9dfd4a4
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-02-13 07:36:21 -0800

    Set up views

--- get-post.js
+++ get-post.js
@@ -8,10 +8,10 @@ are only so many ways to do these things :)
 *********************************************************/
 
 // Import express
-
 var express = require('express');
 var app = express();
 
+// Set up handlebars
 var handlebars = require('express-handlebars').create({
     defaultLayout:'main',
     helpers: {
@@ -26,12 +26,14 @@ var handlebars = require('express-handlebars').create({
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-// form processing:
-// app.use(require('body-parser')()); // makes req.body available
 
 // Set port and public folder for static content
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + 'public'));
+// form processing:
+// app.use(require('body-parser')()); // makes req.body available
+
+
 
 /******************
 * ROUTES:         *
@@ -43,23 +45,24 @@ app.get('/', function(req,res){
 });
 
 
-// Error Pages
-//app.use(function(req, res, next){
-//   res.status(404);
-//    res.render('404');
-//});
-//
-//app.use(function(err, req, res, next) {
-//    console.error(err.stack);
-//    res.status(500);
-//    res.render('500');
-//});
+// Error Pages 404 (Not found) & 500 (Error)
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
 
 /******************
 * Start Server    *
 ******************/
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' +
-app.get('port') + '; press Ctrl-C to terminate.' );
+  console.log('Express started on http://52.26.146.27:' + 
+              app.get('port') + 
+              '; press Ctrl-C to terminate.' );
 });
